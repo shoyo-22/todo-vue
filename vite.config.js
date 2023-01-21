81c39e723ee1fca90ae06faad89ef9b0068cd247
Yessen Murat <shoyo2209@gmail.com>
2023-01-21 22:56:43 +0600

    Added primary color values and minor changes

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/scss/main.scss";`,
+        additionalData: `@use "@/scss/main.scss" as *;`,
       },
     },
   },
