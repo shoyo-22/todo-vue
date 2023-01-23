f63396700822b8ce41a38ef2fb5cbbcd4ce7e86c
Yessen Murat <shoyo2209@gmail.com>
2023-01-23 19:51:45 +0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,46 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+let tasks = ref([]);
+
+const addTaskForm = ref({
+  title: "",
+  description: "",
+});
+const addTask = () => {
+  tasks.value.push(addTaskForm.value);
+};
+</script>
 
 <template>
-  <main>Todo App in Vue</main>
+  <header>Todo Application</header>
+  <main>
+    <form @submit.prevent="addTask">
+      <input
+        type="text"
+        v-model="addTaskForm.title"
+        placeholder="Enter title"
+      />
+      <textarea
+        v-model="addTaskForm.description"
+        cols="30"
+        rows="10"
+        placeholder="Enter description"
+      >
+      </textarea>
+      <button type="submit">Save</button>
+    </form>
+    {{ tasks }}
+  </main>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+form {
+  max-width: 400px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+}
+</style>
